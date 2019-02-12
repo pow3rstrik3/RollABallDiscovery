51ea6f0896ec2269b23a12f6f43524d347d3e7f8
Dailen Hertog <hertogdailen@hotmail.nl>
2019-02-12 16:28:15 +0100

    Adds Toggle Mechanic

---
+++ Roll A Ball Discovery/Assets/Scripts/ToggleEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleEvent : MonoBehaviour
+{
+    public Object prefabOfObject;
+    public Vector3 positionOfToggledObject;
+    public Quaternion rotationOfToggledObject;
+    private Object toggledObject;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject otherObject = collision.collider.gameObject;
+
+        if (otherObject.CompareTag("Player"))
+        {
+            EnterToggleEvent();
+        }
+    }
+
+    private void EnterToggleEvent()
+    {
+        if (toggledObject == null)
+        {
+            Debug.Log("Create");
+            toggledObject = Object.Instantiate(prefabOfObject, positionOfToggledObject, rotationOfToggledObject);
+        }
+        else if (toggledObject != null)
+        {
+            Debug.Log("Destroy");
+            Destroy(toggledObject);
+            toggledObject = null;
+        }
+    }
+}
