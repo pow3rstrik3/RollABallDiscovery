66220af3659e5a659dcc8de9a851ba8e5acfce86
Mitch peters <M.peters12@student.han.nl>
2019-02-12 16:12:47 +0100

    Added ElevatorController

---
+++ Roll A Ball Discovery/Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private bool up = true;
+
+    public int startHeight;
+    public int endHeight;
+    public int maxSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        MoveVertical();
+    }
+
+    void MoveVertical()
+    {
+        var NewPosistion = transform.position;
+        if (up == true)
+        {
+            NewPosistion.y += (maxSpeed * Time.deltaTime);
+            transform.position = NewPosistion;
+            if (transform.position.y >= endHeight)
+            {
+                up = false;
+            }
+        }
+        if (up == false) {
+            NewPosistion.y -= (maxSpeed * Time.deltaTime);
+            transform.position = NewPosistion;
+            if (transform.position.y <= startHeight)
+            {
+                up = true;
+            }
+        }
+    }
+}
